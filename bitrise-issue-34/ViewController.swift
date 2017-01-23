6f01d7ee59e9af036156a14547490bbb7f482404
Justin Makaila <justinmakaila@gmail.com>
2017-01-23 13:16:05 -0500

    Added empty xcodeproj that depends on Carthage dependencies

---
+++ bitrise-issue-34/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitrise-issue-34
+//
+//  Created by Justin Makaila on 1/23/17.
+//  Copyright © 2017 justinmakaila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
